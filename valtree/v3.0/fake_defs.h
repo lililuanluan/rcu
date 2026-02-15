fce00495b8b626e824ee070778a4bca99d2a1079
luanli <3352760396@qq.com>
2026-02-15 22:57:06 +0100

    use stdbool atomic*_t += _Atomic

--- valtree/v3.0/fake_defs.h
+++ valtree/v3.0/fake_defs.h
@@ -25,6 +25,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <stdatomic.h>
 
 /* Definitions taken from the Linux Kernel (v.3.0) */
 
@@ -145,20 +147,20 @@
 
 /* Atomic data types */
 typedef struct {
-	int counter;
+	_Atomic int counter;
 } atomic_t;
 
 typedef struct {
-	long counter;
+	_Atomic long counter;
 } atomic_long_t;
 
 /* Boolean data types */
-typedef _Bool bool;
+// typedef _Bool bool;
 
-enum {
-	false	= 0,
-	true	= 1
-};
+// enum {
+// 	false	= 0,
+// 	true	= 1
+// };
 
 /* Integer types */
 typedef unsigned long ulong;
